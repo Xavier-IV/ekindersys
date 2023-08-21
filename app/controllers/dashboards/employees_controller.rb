2c059a37696565af7aa259027e52d35634704a1b
Zafranudin Zafrin <mohd.zafranudin@gmail.com>
2023-08-21 17:58:40 +0200

    fix: Lint

---
+++ app/controllers/dashboards/employees_controller.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+module Dashboards
+  class EmployeesController < DashboardsController
+  end
+end
---
+++ app/helpers/dashboards/employees_helper.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+module Dashboards
+  module EmployeesHelper
+  end
+end
